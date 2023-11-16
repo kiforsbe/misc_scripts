c0d10c0155270097a72e6ad7dd2b725025776068
kiforsbe <kim.forsberg@gmail.com>
2023-11-16 16:46:06 +0100

    Initial commit.

---
+++ srt_to_transcript.py
@@ -0,0 +1,93 @@
+import srt
+import os
+import sys
+import getopt
+import glob
+
+def getOpts(argv):
+    # Set some output defaults
+    inputPath = "*.srt"
+    outputPath = "out"
+    verbose=False
+
+    # Check command line args, and if available, override the defaults
+    try:
+        opts, args = getopt.getopt(argv, "hi:o:v", ["input=", "output="])
+    except getopt.GetoptError:
+        print(r"srt_to_transcript.py [-i <inputPath>] [-o <outputPath>] [-v]")
+        sys.exit(2)
+
+    # Walk through each option and arg
+    for opt, arg in opts:
+        if opt == '-h':
+            printHelp()
+            sys.exit()
+        elif opt in ("-i", "--input"):
+            inputPath = arg
+        elif opt in ("-o", "--output"):
+            outputPath = arg
+        elif opt in ("-v"):
+            verbose = True
+    
+    # Return final output, either defaults or overridden ones
+    return inputPath, outputPath, verbose
+
+def printHelp():
+    print(r"srt_to_transcript.py [-i <inputPath>] [-o <outputPath>] [-f <ffmpegPath>] [-v]")
+    print(r"")
+    print(r"Batch convert all specified .srt files to .txt.")
+    print(r"")
+    print(r"Command-line options:")
+    print(r"-i path")
+    print(r"--input=path")
+    print(r"    Path to files to operate on. Wildcards can be applied. [default=*.ogg].")
+    print(r"")
+    print(r"-o path")
+    print(r"--output=path")
+    print(r"    Path to output of files [default='out/'].")
+    print(r"")
+    print(r"-v")
+    print(r"    Enable verbose output.")
+
+def main(argv):
+    # Get options
+    inputPath, outputPath, verbose = getOpts(argv)
+
+    # Normalize paths
+    inputPath = os.path.normpath(inputPath)
+    outputPath = os.path.normpath(outputPath)
+
+    # If output path does not exist, create it
+    if not os.path.exists(outputPath):
+        os.mkdir(outputPath)
+
+    # Get list of all files mathing inputPath (can use wild cards, e.g. "inputpath\*.mp4")
+    files = glob.glob(inputPath)
+
+    # Create instance of the model
+
+
+    # Execute operation on each file
+    for file in files:
+        outputFile = os.path.join(outputPath, f"{os.path.splitext(os.path.basename(file))[0]}.txt")
+        subtitles = []
+
+        # Strip the srt and combine sentences
+        if(verbose): print(f"Transcribing: '{file}'... ", end="", flush=True)
+
+        # Read subtitles and save stipped version
+        with open(file) as fp:
+            subtitle_generator = srt.parse(fp)
+            subtitles = list(subtitle_generator)
+            
+        # Write subtitles to plain text file
+        with open(outputFile, 'w') as fp:
+            for item in subtitles:
+                # write each item on a new line
+                fp.write("%s\n" % item.content)
+        
+        if(verbose): print(f"Done")
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
---
+++ transcribe_to_srt.py
@@ -0,0 +1,83 @@
+from subsai import SubsAI
+import os
+import sys
+import getopt
+import glob
+
+def getOpts(argv):
+    # Set some output defaults
+    inputPath = ""
+    outputPath = "out"
+    verbose=False
+
+    # Check command line args, and if available, override the defaults
+    try:
+        opts, args = getopt.getopt(argv, "hi:o:v", ["input=", "output="])
+    except getopt.GetoptError:
+        print(r"transcribe.py [-i <inputPath>] [-o <outputPath>] [-v]")
+        sys.exit(2)
+
+    # Walk through each option and arg
+    for opt, arg in opts:
+        if opt == '-h':
+            printHelp()
+            sys.exit()
+        elif opt in ("-i", "--input"):
+            inputPath = arg
+        elif opt in ("-o", "--output"):
+            outputPath = arg
+        elif opt in ("-v"):
+            verbose = True
+    
+    # Return final output, either defaults or overridden ones
+    return inputPath, outputPath, verbose
+
+def printHelp():
+    print(r"transcribe.py [-i <inputPath>] [-o <outputPath>] [-f <ffmpegPath>] [-v]")
+    print(r"")
+    print(r"Transcribe all specified media files to .srt.")
+    print(r"")
+    print(r"Command-line options:")
+    print(r"-i path")
+    print(r"--input=path")
+    print(r"    Path to files to operate on. Wildcards can be applied. [default=*.ogg].")
+    print(r"")
+    print(r"-o path")
+    print(r"--output=path")
+    print(r"    Path to output of files [default='out/'].")
+    print(r"")
+    print(r"-v")
+    print(r"    Enable verbose output.")
+
+def main(argv):
+    # Get options
+    inputPath, outputPath, verbose = getOpts(argv)
+
+    # Normalize paths
+    inputPath = os.path.normpath(inputPath)
+    outputPath = os.path.normpath(outputPath)
+
+    # If output path does not exist, create it
+    if not os.path.exists(outputPath):
+        os.mkdir(outputPath)
+
+    # Get list of all files mathing inputPath (can use wild cards, e.g. "inputpath\*.mp4")
+    files = glob.glob(inputPath)
+
+    # Create instance of the model
+    subs_ai = SubsAI()
+    model = subs_ai.create_model('m-bain/whisperX', {'model_type': 'base', 'device': 'cuda', 'language': 'en'})
+
+    # Execute operation on each file
+    for file in files:
+        outputFile = os.path.join(outputPath, f"{os.path.splitext(os.path.basename(file))[0]}.srt")
+
+        # Transcribe the file
+        if(verbose): print(f"Transcribing: '{file}'... ", end="", flush=True)
+        subs = subs_ai.transcribe(file, model)
+        subs.save(outputFile)
+        if(verbose): print(f"Done")
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
